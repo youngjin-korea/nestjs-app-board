a450f4aea2bda32b00e3201bef205dc29e7b1095
YoungJinKim <youngjin1293@naver.com>
2024-08-11 16:51:03 +0900

    [feat] pip customer -> patch Status를 변경하는 기능에 받은 변경할 상태값의 유효성을 체크하고 메세지를 보내는 기능 추가

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -12,6 +12,7 @@ import {
 import { BoardsService } from './boards.service';
 import { Board, BoardStatus } from './board.model';
 import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 
 @Controller('boards')
 export class BoardsController {
@@ -49,7 +50,7 @@ export class BoardsController {
   @Patch('/:id/status')
   updateBoardsStatus(
     @Param('id') id: string,
-    @Body('status') status: BoardStatus,
+    @Body('status', BoardStatusValidationPipe) status: BoardStatus,
   ): Board {
     return this.boardsService.updateBoardStatus(id, status);
   }
---
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,27 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+} from '@nestjs/common';
+import { BoardStatus } from '../board.model';
+
+export class BoardStatusValidationPipe implements PipeTransform {
+  readonly StatusOptions: BoardStatus[] = [
+    BoardStatus.PRIVATE,
+    BoardStatus.PUBLIC,
+  ];
+
+  transform(value: any, metadata: ArgumentMetadata) {
+    value = value.toUpperCase();
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`${value} is not in the status options`);
+    }
+    return value;
+  }
+
+  private isStatusValid(status: any) {
+    // arrays.indexof(값)인 경우에 포함되지 않으면 -1을 리턴함
+    const index = this.StatusOptions.indexOf(status);
+    return index !== -1;
+  }
+}
